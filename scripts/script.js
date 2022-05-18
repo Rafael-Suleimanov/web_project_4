eb222fa4109cc3fe6aae2a1568b6e6d354688570
Rafael Suleimanov <rafa.suleimanov777@gmail.com>
2022-05-17 20:57:40 -0400

    new update

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditPopup = document.querySelector(".popup");
-const formEditCloseIcon = document.querySelector(".form__close-icon");
+const formEditCloseIcon = document.querySelector(".popup__close-icon");
 const profileEditForm = document.querySelector("#form");
 const profileNameElement = document.querySelector(".profile__name");
 const profileJobElement = document.querySelector(".profile__job");
-const profileNameInput = form.querySelector(".form__input_name");
-const profileJobInput = form.querySelector(".form__input_job");
+const profileNameInput = form.querySelector(".form__input-name");
+const profileJobInput = form.querySelector(".form__input-job");
 const cardEditIcon = document.querySelector(".card__icon");
 
 function openPopup() {
